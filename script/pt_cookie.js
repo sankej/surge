51ce1928f0a947eb7b1a7112cb871a373896e9e4
sankej <jasonlovetavia@gmail.com>
2021-11-03 07:07:34 +0800

    update

--- script/pt_cookie.js
+++ script/pt_cookie.js
@@ -10,16 +10,16 @@ const getCookie = (cookieKey) => {
   try {
     const cookie = $request.headers.Cookie
     const preCookie = $persistentStore.read(cookieKey)
-    const compareKey = !!cookie
-      ? /cf_clearance=([a-z_A-Z0-9-\.!@#\$%\\\^&\*\)\(\+=\{\}\[\]\/",'<>~\·`\?:|]*)/.exec(cookie)[1]
-      : null
-    const comparePreKey = !!preCookie
-      ? /cf_clearance=([a-z_A-Z0-9-\.!@#\$%\\\^&\*\)\(\+=\{\}\[\]\/",'<>~\·`\?:|]*)/.exec(
-          preCookie,
-        )[1]
-      : null
+    // const compareKey = !!cookie
+    //   ? /cf_clearance=([a-z_A-Z0-9-\.!@#\$%\\\^&\*\)\(\+=\{\}\[\]\/",'<>~\·`\?:|]*)/.exec(cookie)[1]
+    //   : null
+    // const comparePreKey = !!preCookie
+    //   ? /cf_clearance=([a-z_A-Z0-9-\.!@#\$%\\\^&\*\)\(\+=\{\}\[\]\/",'<>~\·`\?:|]*)/.exec(
+    //       preCookie,
+    //     )[1]
+    //   : null
 
-    if (!preCookie || comparePreKey !== compareKey) {
+    if (!preCookie || preCookie !== cookie) {
       $persistentStore.write(cookie, cookieKey)
       log(`旧的Cookie: ${preCookie}\n新的Cookie: ${cookie}\nCookie不同，写入新的Cookie成功！`)
       notify('Cookie写入成功!')
