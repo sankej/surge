c26857db0541a8f76eaa38547704521dfd25613a
sankej <jasonlovetavia@gmail.com>
2021-11-03 06:55:52 +0800

    update

--- script/pt_cookie.js
+++ script/pt_cookie.js
@@ -8,9 +8,7 @@ const log = (msg, type = 'INFO') => console.log(`[${type}] [${scriptName}]\n${ms
 
 const getCookie = (cookieKey) => {
   try {
-    const cookie = $request.headers.cookie
-    console.log(cookie)
-    console.log(cookieKey)
+    const cookie = $request.headers.Cookie
     const preCookie = $persistentStore.read(cookieKey)
     const compareKey = !!cookie
       ? /cf_clearance=([a-z_A-Z0-9-\.!@#\$%\\\^&\*\)\(\+=\{\}\[\]\/",'<>~\·`\?:|]*)/.exec(cookie)[1]
