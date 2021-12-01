1bab1cfc79a8ff72f58da85f7175caca1982139c
sankej <jasonlovetavia@gmail.com>
2021-12-02 04:37:07 +0800

    update

--- script/gist_sync.js
+++ script/gist_sync.js
@@ -2,8 +2,6 @@ const scriptName = 'Gist_Sync'
 
 const notify = (msg) => $notification.post(scriptName, '', msg)
 const v4IP = $network.v4.primaryAddress
-const script_text =
-  "const scriptName :  'Gist_Sync'\nconst notify = (msg) => $notification.post(scriptName, '', msg)\nconst log = (msg, type = 'INFO') => console.log(`[${type}] [${scriptName}]\\n${msg}\\n`)\n\nconst getScript = () => {\n  return new Promise((resolve, reject) => {\n    $httpClient.get(\n      {\n        url: 'https://raw.githubusercontent.com/sankej/surge/main/script/gist_restore.js',\n      },\n      (err, response, data) => {\n        if (err || response.status !== 200) {\n          log(`获取脚本失败, 请求异常: ${err}`, 'ERROR')\n          reject('获取脚本失败，请求异常, 请查阅日志!')\n        } else {\n          resolve(data)\n        }\n      },\n    )\n  })\n}\n\n;(async () => {\n  try {\n    const script = await getScript()\n    eval(script)\n  } catch (error) {\n    notify(error)\n  }\n  $done()\n})()"
 
 const sync = (url) => {
   return new Promise((resolve, reject) => {
@@ -14,9 +12,10 @@ const sync = (url) => {
         Accept: '*/*',
       },
       body: {
-        script_text,
+        script_text:
+          "const scriptName = 'Gist_Sync'\nconst notify = (msg) => $notification.post(scriptName, '', msg)\nconst log = (msg, type = 'INFO') => console.log(`[${type}] [${scriptName}]\\n${msg}\\n`)\n\nconst getScript = () => {\n  return new Promise((resolve, reject) => {\n    $httpClient.get(\n      {\n        url: 'https://raw.githubusercontent.com/sankej/surge/main/script/gist_restore.js',\n      },\n      (err, response, data) => {\n        if (err || response.status !== 200) {\n          log(`获取脚本失败, 请求异常: ${err}`, 'ERROR')\n          reject('获取脚本失败，请求异常, 请查阅日志!')\n        } else {\n          resolve(data)\n        }\n      },\n    )\n  })\n}\n\n;(async () => {\n  try {\n    const script = await getScript()\n    eval(script)\n  } catch (error) {\n    notify(error)\n  }\n  $done()\n})()",
         mock_type: 'cron',
-        timeout: 30,
+        timeout: 5,
       },
     }
 
